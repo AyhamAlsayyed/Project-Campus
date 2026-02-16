0db20eda82a41a896aa2cdb449794a2433322792
Ayham Alsayyed <ayham.sayyed.04@gmail.com>
2026-02-16 22:50:39 +0200

    Initial project setup

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import LandingPage from './components/landingpage/LandingPage';
+import Login from './components/authentication/login';
+import Signup from './components/authentication/signup';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+         <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </BrowserRouter>
+
+  );
+}
+
+export default App;
